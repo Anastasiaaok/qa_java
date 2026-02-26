80956a3c930f4f03af1e800fbc5493bc52214439
Anastasia <anastasiaaok1996@gmail.com>
2026-02-26 13:53:34 +0300

    add tests, dependency injection, mocks, parameterized tests, jacoco report

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,4 +1,33 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
 public class CatTest {
+
+    @Test
+    public void getSoundTest() {
+        Feline feline = Mockito.mock(Feline.class);
+        Cat cat = new Cat(feline);
+
+        Assert.assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+
+        Mockito.when(feline.eatMeat())
+                .thenReturn(List.of("Животные"));
+
+        Cat cat = new Cat(feline);
+
+        Assert.assertEquals(
+                List.of("Животные"),
+                cat.getFood()
+        );
+    }
 }
--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,4 +1,38 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class FelineTest {
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> food = feline.eatMeat();
+
+        Assert.assertEquals(
+                List.of("Животные", "Птицы", "Рыба"),
+                food
+        );
+    }
+
+    @Test
+    public void getFamilyTest() {
+        Feline feline = new Feline();
+        Assert.assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensDefaultTest() {
+        Feline feline = new Feline();
+        Assert.assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithParameterTest() {
+        Feline feline = new Feline();
+        Assert.assertEquals(5, feline.getKittens(5));
+    }
 }
